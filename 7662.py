6476e409cc4a55e815a351548e65af2d013e9efc
정준환 <zxxzx1515@naver.com>
2022-04-10 21:53:34 +0900

    7662 푸는중

---
+++ 7662.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+
+Q = deque()
+
+
+# 테스트 케이스 개수
+T = int(sys.stdin.readline().strip())
+
+# Q에 적용할 연산의 개수
+k = int(sys.stdin.readline().strip())
+
+for _ in range(k):
+    DI, n = sys.stdin.readline().split()
+
+
+
+    pass
+
+
+# I일때 하는 일
+def I():
+
+
+# D일때 하는 일
+def D(Q, DI):
+    Q.empty
